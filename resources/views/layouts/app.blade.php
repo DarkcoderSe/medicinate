5ce8d283f18bf92d5da6499e44c9d3b01fa3cd0d
DarkcoderSe <kashifs294@gmail.com>
2021-05-29 18:28:49 +0500

    static changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,123 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>
+            @yield('title')Online Donation Unused Medicines for NGOs
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{asset('admin/assets/images/favicon.ico')}}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" href="{{asset('admin/assets/libs/owl.carousel/assets/owl.carousel.min.css')}}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{asset('admin/assets/libs/owl.carousel/assets/owl.theme.default.min.css')}}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Bootstrap Css -->
+        <link href="{{asset('admin/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{asset('admin/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    </head>
+
+    <body data-spy="scroll" data-target="#topnav-menu" data-offset="60">
+
+        <nav class="navbar navbar-expand-lg navigation fixed-top sticky">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-logo" href="{{ URL::to('/') }}">
+                    <h3 class="text-white">
+                        Project Name
+                    </h3>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+
+                <button type="button" class="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+                    <i class="fa fa-fw fa-bars"></i>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+              
+                <div class="collapse navbar-collapse" id="topnav-menu-content">
+                    <ul class="navbar-nav ml-auto" id="topnav-menu" >
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ URL::to('/') }}">Home</a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('donate') }}">Donate Medicines</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('donate/history') }}">My Donations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('report-issue') }}">Report Issue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('feedback') }}">Feedback</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('/#faqs') }}">FAQs</a>
+                        </li>
 
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    @guest
+                    <div class="ml-lg-2">
+                        <a href="{{ URL::to('login') }}" class="btn btn-outline-success w-xs">Login</a>
+                    </div>
+                    <div class="ml-lg-2">
+                        <a href="{{ URL::to('register') }}" class="btn btn-link text-light w-xs">Register</a>
+                    </div>
+                    @else 
+                    <div class="ml-lg-2">
+                        <a href="javascript:void(0);" class="btn btn-link text-light w-xs">{{ auth()->user()->name ?? '' }}</a>
+                    </div>
+                    @endguest
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        @yield('content')
+
+        <!-- Footer start -->
+        <footer class="landing-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mb-4">
+                            <img src="assets/images/logo-light.png" alt="" height="20">
+                        </div>
+    
+                        <p class="mb-2">2020 © Online Unused Medicine Donation for NGOs</p>
+                    </div>
+
+                </div>
+            </div>
+            <!-- end container -->
+        </footer>
+        <!-- Footer end -->
+
+        <!-- JAVASCRIPT -->
+        <script src="{{URL::to('admin/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{URL::to('admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{URL::to('admin/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{URL::to('admin/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{URL::to('admin/assets/libs/node-waves/waves.min.js')}}"></script>
+
+        <script src="{{URL::to('admin/assets/libs/jquery.easing/jquery.easing.min.js')}}"></script>
+
+        <!-- Plugins js-->
+        <script src="{{URL::to('admin/assets/libs/jquery-countdown/jquery.countdown.min.js')}}"></script>
+
+        <!-- owl.carousel js -->
+        <script src="{{URL::to('admin/assets/libs/owl.carousel/owl.carousel.min.js')}}"></script>
+
+        <!-- ICO landing init -->
+        <script src="{{URL::to('admin/assets/js/pages/ico-landing.init.js')}}"></script>
+
+        <script src="{{URL::to('admin/assets/js/app.js')}}"></script>
+
+    </body>
 </html>
