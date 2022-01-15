154b7230d27b1d27cb21ac6a8e9c6be7ce612720
Ahmadi2021 <ahmadi202137@gmail.com>
2022-01-15 13:45:22 +0500

    add strip

--- app/Http/Controllers/StripController/StripController.php
+++ app/Http/Controllers/StripController/StripController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\StripController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Stripe;
+use Session;
 class StripController extends Controller
 {
     public  function purchasForm(){
         return view('StripPayment.show');
     }
     public function purchas(Request $request){
+
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         Stripe\Charge::create ([
             "amount" => 100 * 100,
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                         <li class="nav-item">
                             <a class="nav-link active" href="{{ URL::to('/') }}">Home</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ URL::to('/') }}">Donate Amount</a>
+                        </li>
 
                         <li class="nav-item">
                             <a class="nav-link" href="{{ URL::to('donation') }}">Donate Medicines</a>
